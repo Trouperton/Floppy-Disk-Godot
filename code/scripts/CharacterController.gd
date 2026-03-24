85f7b0228720a77b18f0f400543760c487e6d98f
Trouperton <58820346+Trouperton@users.noreply.github.com>
2026-03-24 22:06:03 +0000

    Removed prototype assets and created basic prototype.
    Unfortunately the Kenney prototype pack didn't have the pivots in the centre of the bits so I couldn't use them without jankiness.

---
+++ code/scripts/CharacterController.gd
@@ -0,0 +1,14 @@
+extends CharacterBody3D
+
+const JUMP_VELOCITY = 4.5
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept"):
+		velocity.y = JUMP_VELOCITY
+
+	move_and_slide()
