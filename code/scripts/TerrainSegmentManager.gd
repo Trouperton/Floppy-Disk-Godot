87d4e402bf48e78c0a8abaff298f667cd6f169c3
Truperton <58820346+Trouperton@users.noreply.github.com>
2026-03-28 12:36:58 +0000

    Terrain manager now moves all the segments.
    Should be more optimised than having all the segments have their own scripts that won't be really doing anything else.
    
    Also prepares for when the manager script will be used to spawn extra segments.

--- code/scripts/TerrainSegmentManager.gd
+++ code/scripts/TerrainSegmentManager.gd
@@ -1,11 +1,16 @@
-extends GridMap
+extends Node3D
+
+var terrain_segments
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	terrain_segments = get_children()
+	print(terrain_segments)
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	#position.x -= 1 * delta
+	for segment in terrain_segments:
+		segment.position.x -= 1 * delta
 	pass
