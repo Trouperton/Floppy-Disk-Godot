ad0cdb6165514fec90fd0a993d9b7e950f8e35e3
Truperton <58820346+Trouperton@users.noreply.github.com>
2026-03-28 12:25:49 +0000

    Added a rudimentary lose condition.
    To that effect I created an "obstacle" group to put on objects that would make the player lose.
    
    And in the character controller script I added a check for the new group which has the scene get reset.

--- code/scripts/CharacterController.gd
+++ code/scripts/CharacterController.gd
@@ -4,7 +4,11 @@ const JUMP_VELOCITY = 4.5
 
 func _physics_process(delta: float) -> void:
 	if get_slide_collision_count() > 0:
-		print(get_slide_collision(0).get_collider(0).name)
+		print(get_slide_collision(0).get_collider(0).get_groups())
+		for group in get_slide_collision(0).get_collider(0).get_groups():
+			if group == "obstacle":
+				print("lose")
+				get_tree().reload_current_scene()
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
