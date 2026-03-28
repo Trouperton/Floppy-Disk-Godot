48a8a7f8141bd0f427233ddd872472cbfe7fdf92
Truperton <58820346+Trouperton@users.noreply.github.com>
2026-03-28 21:25:58 +0000

    Added a check that will be used to add points to the score.
    Added area3Ds to the segments that have a script that checks if they player entered the threshold and if they did delete itself. Will later add some way for them to add to the score.
    
    Also created a player group to accommodate this to avoid triggering incorrectly.

---
+++ code/scripts/ScoreThreshold.gd
@@ -0,0 +1,24 @@
+extends Area3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_area_entered(area: Area3D) -> void:
+	print("Threshold reached")
+	print(Area3D.name)
+	pass # Replace with function body.
+
+func _on_body_entered(body: Node3D) -> void:
+	for group in body.get_groups():
+		if group == "player":
+			print("score +1")
+			self.queue_free()
+	pass # Replace with function body.
