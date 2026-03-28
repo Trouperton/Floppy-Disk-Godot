9881f1509fbbf75ec166affcdabe12c9b5824cf2
Truperton <58820346+Trouperton@users.noreply.github.com>
2026-03-28 11:09:45 +0000

    Added a collision checker that outputs other object's name.
    Simple if statement that checks if the collision count is greater than 0 followed by a print that gets the object's name.

--- code/scripts/CharacterController.gd
+++ code/scripts/CharacterController.gd
@@ -3,6 +3,8 @@ extends CharacterBody3D
 const JUMP_VELOCITY = 4.5
 
 func _physics_process(delta: float) -> void:
+	if get_slide_collision_count() > 0:
+		print(get_slide_collision(0).get_collider(0).name)
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
