29cb22982d4160059e7d419acab23bc356f10df6
Trouperton <58820346+Trouperton@users.noreply.github.com>
2026-04-06 22:11:55 +0100

    Fixed bug that allowed opening pause menu in end screen.
    Added an extra condition to if statement that prevents opening the pause menu if the end screen is visible.
    
    The bug was problematic in particular because you could unpause the end screen making the player drop down by opening and closing the screen.

--- code/scripts/PauseScreen.gd
+++ code/scripts/PauseScreen.gd
@@ -3,7 +3,7 @@ extends Control
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("pause"):
+	if Input.is_action_just_pressed("pause") and !$"../EndScreen".visible:
 		visible = !visible
 
 
