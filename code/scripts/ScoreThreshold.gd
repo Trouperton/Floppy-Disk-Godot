b64c7a904151cbb04e7307939de3e69978a89f00
Trouperton <58820346+Trouperton@users.noreply.github.com>
2026-04-10 18:18:30 +0100

    Removed an unnecessary variable from score threshold script.

--- code/scripts/ScoreThreshold.gd
+++ code/scripts/ScoreThreshold.gd
@@ -1,11 +1,10 @@
 extends Area3D
 
 signal score_threshold_triggered(points: int)
-@onready var level_manager = $"../../../.."
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	self.score_threshold_triggered.connect(level_manager._on_score_threshold_triggered)
+	self.score_threshold_triggered.connect($"../../../.."._on_score_threshold_triggered)
 
 
 func _on_body_entered(body: Node3D) -> void:
