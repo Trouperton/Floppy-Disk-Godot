8478015d139473be7272a3f68c029ca4cc9fbd4c
Trouperton <58820346+Trouperton@users.noreply.github.com>
2026-04-22 21:26:28 +0100

    Added complete documentation for the terrain segment manager script.
    I have adhered to the documentation comments structure that is present in Godot.
    
    This should help maintainability, since there are links to members.

--- code/scripts/TerrainSegmentManager.gd
+++ code/scripts/TerrainSegmentManager.gd
@@ -5,7 +5,11 @@ extends Node3D
 @export var base_terrain_speed: float = 1.0
 
 @export_category("Terrain Spawning")
+## The distance to the left from the centre of the screen at which terrain segments get
+## deleted to avoid major performance impact.
 @export var terrain_delete_threshold: float = -30
+## The distance to the right from the centre of the screen, if there is terrain
+## missing to the left of this point; more terrain gets spawned.
 @export var terrain_spawning_threshold: float = 30
 ## The minimum number of segments that will be spawned whenever the manager spawns more.
 @export var minimum_segments_to_spawn: int = 5
@@ -13,7 +17,9 @@ extends Node3D
 @export var terrain_segment_spawn_collection: Array[PackedScene]
 
 
+## The current speed at which the terrain gets moved each physics process.
 var terrain_speed: float
+## The collection of terrain segments that the manager is currently managing.
 var terrain_segments
 
 
@@ -28,12 +34,15 @@ func _physics_process(delta: float) -> void:
 	move_terrain(delta)
 
 
+## Moves all terrain segments to the left based on [member terrain_speed].
 func move_terrain(delta: float):
 	for segment in terrain_segments:
 		segment.position.x -= terrain_speed * delta
 
 
 #region Terrain Checking
+## Looks for all the child ones of the empty "TerrainSegments" node and adds them
+## to [member terrain_segments].
 func find_segments():
 	terrain_segments = $TerrainSegments.get_children()
 	#print_debug(name, " node found ", terrain_segments)
@@ -41,6 +50,12 @@ func find_segments():
 		printerr(name, " failed to find terrain segments!")
 
 
+## Checks what [member terrain_segments] are currently present.[br][br]
+## 
+## Any segments past the [member terrain_delete_threshold], get deleted.[br][br]
+##
+## And if there isn't enough terrain segments ahead of the [member terrain_spawning_threshold],
+## spawns more based on [member minimum_segments_to_spawn].
 func check_terrain():
 	var segments_changed = false
 	var furthest_forward: GridMap = null
