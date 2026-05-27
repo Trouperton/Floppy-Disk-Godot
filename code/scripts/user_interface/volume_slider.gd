3676d595b971b47efcf6829095132ce6ee0de372
Trouperton <58820346+Trouperton@users.noreply.github.com>
2026-05-27 21:57:42 +0100

    Changed _ready() to read current bus values rather than set them.
    This is to prevent a situation if I were to add the same sliders to the level, since that would cause issues with the settings not mattering after setting them in the main menu.
    
    Because with previous implementation it'd just reset the volume to the slider default.

--- code/scripts/user_interface/volume_slider.gd
+++ code/scripts/user_interface/volume_slider.gd
@@ -6,7 +6,12 @@ extends HSlider
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	AudioServer.set_bus_volume_db(AudioServer.get_bus_index(bus_index), -60 + (60 * value/100))
+	var bus_volume_db = AudioServer.get_bus_volume_db(AudioServer.get_bus_index(bus_index))
+	
+	if bus_volume_db == 0:
+		value = 100
+	else:
+		value = (bus_volume_db + 60) / 60 * 100
 
 
 func _on_value_changed(value: float) -> void:
