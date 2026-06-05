71ba2e9dc9c6d740a3c8095c3771a7bb2789e29f
Trouperton <58820346+Trouperton@users.noreply.github.com>
2026-06-05 17:02:10 +0100

    Can return to main menu from pause menu now, also fixed a bug.
    Previously used packed scenes, but those would sometimes become null when loading a scene.
    
    To resolve that now instead we load from file and store the file path instead. unfortunately less convenient, but more reliable.

--- code/scripts/user_interface/button_scene_transition.gd
+++ code/scripts/user_interface/button_scene_transition.gd
@@ -1,9 +1,10 @@
 extends Button
 
 
-@export var scene_to_open: PackedScene
+@export var scene_to_open: String
 
 
 func _pressed() -> void:
 	print_debug("Loading ", scene_to_open)
-	get_tree().change_scene_to_packed(scene_to_open)
+	get_tree().paused = false
+	get_tree().change_scene_to_file(scene_to_open)
